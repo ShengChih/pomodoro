ceac6c9de68d91df9c2404c2b209e8f2e1558b7f
shengchih <ms0529756@gmail.com>
2021-03-10 08:17:20 +0000

    add: init project

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./index.module.scss"
+
+const App = (props) => {
+  return(
+    <div className={ styles.App }>
+      <h1> Hello, World! </h1>
+    </div>
+  );
+}
+
+export default App;
