07bb58c2e74cef6fa19b1931ed682d0c127b11c0
ShengChih Chu <shengchih.chu@ecloudvalley>
2021-06-16 16:11:16 +0000

    upgrade all package, yarn, storybook

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,50 @@
-const path = require("path");
-const webpack = require("webpack");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.js',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [/*MiniCssExtractPlugin.loader, */"style-loader", 'css-loader', 'sass-loader'],
-      }
-    ]
+        use: [
+          /*MiniCssExtractPlugin.loader, */ 'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"],
+    extensions: ['*', '.js', '.jsx'],
     alias: {
-      Components: path.resolve(__dirname, 'src/components/')
-    }
+      Components: path.resolve(__dirname, 'src/components/'),
+    },
   },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 8080,
-    publicPath: "http://localhost:8080/dist/",
-    hotOnly: true
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin(), new MiniCssExtractPlugin()]
-};
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    /*new MiniCssExtractPlugin(),*/
+  ],
+}
